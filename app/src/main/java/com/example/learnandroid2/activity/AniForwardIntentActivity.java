9e8fd6a5820a8b60484710af1390b0d28d975e0c
robocon321 <robocon321n@gmail.com>
2021-02-01 00:08:22 +0700

    Animation for backward and forward between 2 activity

---
+++ app/src/main/java/com/example/learnandroid2/activity/AniForwardIntentActivity.java
@@ -0,0 +1,14 @@
+package com.example.learnandroid2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AniForwardIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ani_forward_intent);
+    }
+}
